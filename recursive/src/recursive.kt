c8c6a71f3f4449f3ca6aa78a63c12eeff7cbcae1
kuckjwi0928 <skyblue0928@naver.com>
2020-08-17 18:03:19 +0900

    Modified content

--- recursive/src/recursive.kt
+++ recursive/src/recursive.kt
@@ -12,6 +12,12 @@ fun main() {
     println(reverse("kuckjwi"))
     println(toBinary(10))
     println(replicate(3, 5))
+    println(take(3, listOf(1, 2, 3, 4, 5)))
+    println(elem(3, listOf(1, 2, 3, 4, 5)))
+    println(takeSequence(5, repeat(3)))
+    println(zip(listOf(1, 3, 5), listOf(2, 4)))
+    println(quicksort(listOf(5, 100, 200, 1, 4, 3)))
+    println(gcd(1112, 695))
 }
 
 fun sum(n: Int): Int = when {
@@ -50,4 +56,41 @@ fun toBinary(n: Int): String = n.toString(2)
 fun replicate(n: Int, element: Int): List<Int> = when {
     n <= 0 -> listOf()
     else -> listOf(element) + replicate(n - 1, element)
+}
+
+fun take(n: Int, list: List<Int>): List<Int> = when {
+    n <= 0 || list.isEmpty() -> listOf()
+    else -> listOf(list.head()) + take(n - 1, list.tail())
+}
+
+fun elem(n: Int, list: List<Int>): Boolean = when {
+    list.isEmpty() -> false
+    n == list.head() -> true
+    else -> elem(n, list.tail())
+}
+
+fun repeat(n: Int): Sequence<Int> = generateSequence(n) { it }
+
+fun takeSequence(n: Int, sequence: Sequence<Int>): List<Int> = when {
+    n <= 0 -> listOf()
+    else -> listOf(sequence.first()) + takeSequence(n - 1, sequence)
+}
+
+fun zip(list1: List<Int>, list2: List<Int>): List<Pair<Int, Int>> = when {
+    list1.isEmpty() || list2.isEmpty() -> listOf()
+    else -> listOf(Pair(list1.head(), list2.head())) + zip(list1.tail(), list2.tail())
+}
+
+fun quicksort(list: List<Int>): List<Int> = when {
+    list.isEmpty() -> listOf()
+    else -> {
+        val pivot = list.head()
+        val partitions = list.tail().partition { pivot > it }
+        quicksort(partitions.first) + listOf(pivot) + quicksort(partitions.second)
+    }
+}
+
+fun gcd(m: Int, n: Int): Int = when (n) {
+    0 -> m
+    else -> gcd(n, m % n)
 }
